9d851eaad9f5a8bf420f4c87d66d509e4141f4ef
leanplumjshek <jonathan.shek@leanplum.com>
2019-07-08 18:06:02 -0700

    finished vigenere.c

--- vigenere.c
+++ vigenere.c
@@ -8,86 +8,90 @@ int main(int argc, string argv[])
 {
     int keyLength = strlen(argv[1]);
     
-  // if two arguments
-  if (argc == 2) 
-  {
-    string key = argv[1];
-
-    // Initialize empty array with length of key
-    int array[keyLength];
-
-    // For character in key
-    for (int i = 0; i < keyLength; i++) 
+    // If two arguments
+     if (argc != 2)
     {
-      // If input is char, store into empty array
-      if (isalpha(key[i]))
-      {
-        array[i] = key[i];  
-      }
-      else 
-      {
         printf("Usage: ./vigenere keyword\n");
-        return 0;
-      }
+        return 1;
     }
-
-    // Prompt user
-    string plaintext = get_string("plaintext: ");
-    int plaintextLength = strlen(plaintext);
-
-    printf("ciphertext: ");
-
-    // Initialize output array 
-    int output[strlen(plaintext)];
-
-    // iterate through length of plaintext
-    for (int i = 0, j = 0; i < plaintextLength; i++)
+    else 
     {
-      // Iterate over key
-      int loc = j % keyLength;
+        string key = argv[1];
 
-      if (islower(plaintext[i]))
-      {
-        if (array[loc] < 'a')
-        {
-          array[loc] = tolower(array[loc]);
-          output[i] = (((plaintext[i] + (array[loc]-'a')) - 'a') % 26) + 'a';
-          j++;
-        }
-        else
-        {
-          output[i] = (((plaintext[i] + (array[loc]-'a')) - 'a') % 26) + 'a';
-          j++;
-        }
-      }
-      else if (isupper(plaintext[i]))
-      {
-        if (array[loc] < 'a')
-        {
-          output[i] = (((plaintext[i] + (array[loc]-'A')) - 'A') % 26) + 'A';
-          j++;
-        }
-        else
-        {
-          array[loc] = toupper(array[loc]);
-          output[i] = (((plaintext[i] + (array[loc]-'A')) - 'A') % 26) + 'A';
-          j++;
-        }
-      }
-      else 
-      {
-        output[i] = plaintext[i];
-      }
+        // Initialize empty array with length of key
+        int array[keyLength];
 
-      printf("%c", output[i]);
+        // For character in key
+        for (int i = 0; i < keyLength; i++) 
+        {
+            // If input is char, store into empty array
+            if (isalpha(key[i]))
+            {    
+                array[i] = key[i];  
+            }
+            else 
+            {
+                printf("Usage: ./vigenere keyword\n");
+                return 1;
+            }
+        }
+
+        // Prompt user
+        string plaintext = get_string("plaintext: ");
+        int plaintextLength = strlen(plaintext);
+
+        printf("ciphertext: ");
+
+        // Initialize output array 
+        int output[strlen(plaintext)];
+
+        // Iterate through length of plaintext
+        for (int i = 0, j = 0; i < plaintextLength; i++)
+        {
+            // Iterate over key
+            int loc = j % keyLength;
+
+            if (islower(plaintext[i]))
+            {
+                // If char in key is uppercase
+                if (array[loc] < 'a')
+                {
+                    // Convert to lower
+                    array[loc] = tolower(array[loc]);
+                    output[i] = (((plaintext[i] + (array[loc] - 'a')) - 'a') % 26) + 'a';
+                    // Increment j after plaintext[i] is encrypted
+                    j++;
+                }
+                else
+                {
+                    output[i] = (((plaintext[i] + (array[loc] - 'a')) - 'a') % 26) + 'a';
+                    j++;
+                }
+            }
+            else if (isupper(plaintext[i]))
+            {
+                if (array[loc] < 'a')
+                {
+                    output[i] = (((plaintext[i] + (array[loc] - 'A')) - 'A') % 26) + 'A';
+                    j++;
+                } 
+                else
+                {
+                    array[loc] = toupper(array[loc]);
+                    output[i] = (((plaintext[i] + (array[loc] - 'A')) - 'A') % 26) + 'A';
+                    j++;
+                }
+            }
+            else 
+            {
+                // If !isalpha, leave as is
+                output[i] = plaintext[i];
+            }
+ 
+            printf("%c", output[i]);
+        }
+    
+        printf("\n");
     }
-    printf("\n");
-  }
-
-  else
-  {
-    printf("Usage: ./vigenere keyword\n");
-    return 0;
-  }
 }
 
